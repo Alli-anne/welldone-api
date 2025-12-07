8e3ac4f34d507cb406977dc60937c4b29cd7328e
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-12-06 21:40:08 -0700

    api changes

--- controller/userController.js
+++ controller/userController.js
@@ -3,35 +3,35 @@ import {getDb} from "../database/connect.js";
 
 export async function getUser(req, res) {
     const db = await getDb();
-    const user = await db.collection('users').findOne({_id: req.params.id});
+    const user = await db.collection('user').findOne({_id: req.params.id});
     res.json(user);
     console.log(user);
 }
 
 export async function getAllUsers(req, res) {
     const db = await getDb();
-    const users = await db.collection('users').find().toArray();
+    const users = await db.collection('user').find().toArray();
     res.json(users);
     console.log(users);
 }
 
 export async function createUser(req, res) {
     const db = await getDb();
-    const user = await db.collection('users').insertOne(req.body);
+    const user = await db.collection('user').insertOne(req.body);
     res.json(user);
     console.log(user);
 }
 
 export async function updateUser(req, res) {
     const db = await getDb();
-    const user = await db.collection('users').updateOne({_id: req.params.id}, {$set: req.body});
+    const user = await db.collection('user').updateOne({_id: req.params.id}, {$set: req.body});
     res.json(user);
     console.log(user);
 }
 
 export async function deleteUser(req, res) {
     const db = await getDb();
-    const user = await db.collection('users').deleteOne({_id: req.params.id});
+    const user = await db.collection('user').deleteOne({_id: req.params.id});
     res.json(user);
     console.log(user);
 }
